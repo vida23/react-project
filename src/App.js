6e3f9b714468ef24b348088754141056890ee5a8
vida23 <vida23>
2022-10-28 02:50:03 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './V R.svg';
 import './App.css';
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <footer>
+        <Footer />
+      </footer>
+      
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    const year = new Date().getFullYear();
+  
+    return <footer>{`Copyright © Vida Rashidi ${year}`}</footer>;
+
+  };
+  
+  export default Footer;
